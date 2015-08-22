b1fd30e313111d41392ca98a6903cadbe6b83a8d
Steve Salmond <steve.salmond@outsmartgames.com>
2015-08-22 16:43:51 +1200

    Working on basic character controls.

---
+++ Assets/Scripts/Damage/Damageable.cs
@@ -0,0 +1,97 @@
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+
+    public float Health = 100;
+
+    public LayerMask DamageMask;
+    public Vector2 DamageSpeedRange = new Vector2(5, 100);
+    public Vector2 DamageRange = new Vector2(5, 100);
+    public GameObject DamageEffect;
+    public GameObject DestroyEffect;
+    public Transform DestroyEffectEmitter;
+
+    public bool DeactivateOnDeath;
+
+    public float DelayBetweenHits = 0.1f;
+
+    public bool FullHealth
+    { get { return Health >= _initialHealth; } }
+
+    public bool Dead
+    { get { return Health <= 0; } }
+
+    // private float _nextHit;
+
+    private float _initialHealth;
+
+    public void AddHealth(float value)
+    {
+        if (Health <= 0)
+            return;
+
+        Health = Mathf.Min(Health + value, _initialHealth);
+    }
+
+    void Start()
+    {
+        _initialHealth = Health;
+
+        if (!DestroyEffectEmitter)
+            DestroyEffectEmitter = transform;
+    }
+
+    /*
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if we can't be hit yet.
+        if (Time.time < _nextHit)
+            return;
+
+        // Check if already dead.
+        if (Health <= 0)
+            return;
+
+        // Can this object hurt us?
+        var go = collision.gameObject;
+        if ((DamageMask.value & 1 << go.layer) == 0)
+            return;
+
+        // Determine how fast the object hit us.
+        var speed = collision.relativeVelocity.magnitude;
+        if (speed < DamageSpeedRange.x)
+            return;
+
+        // Determine how hard we were hit.
+        var range = DamageSpeedRange.y - DamageSpeedRange.x;
+        var strength = Mathf.Clamp01((speed - DamageSpeedRange.x) / range);
+        var damage = Mathf.Lerp(DamageRange.x, DamageRange.y, strength);
+        // Debug.Log("Damaged: (damage = " + damage + ")");
+
+        // Apply damage.
+        Health = Mathf.Max(0, Health - damage);
+        _nextHit = Time.time + DelayBetweenHits;
+
+        // Spawn damage effect.
+        Instantiate(DamageEffect, collision.contacts[0].point, Quaternion.identity);
+
+        // Check if we just died.
+        if (Health <= 0)
+            Die();
+    }
+    */
+
+    void Die()
+    {
+        // Spawn death effect.
+        var t = DestroyEffectEmitter;
+        Instantiate(DestroyEffect, t.position, t.rotation);
+
+        // Kill this object.
+        if (DeactivateOnDeath)
+            gameObject.SetActive(false);
+        else
+            Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,7 +1,149 @@
+using System;
 using UnityEngine;
-using System.Collections;
 
 public class PlayerController : Singleton<PlayerController>
 {
 
+    public float MaxSpeed = 5;
+    public Vector2 InputForceScale = Vector2.one;
+    public Vector2 AirborneForceScale = Vector2.one;
+    public float JumpForce = 1000;
+    public Vector2 OpposingForceScale = Vector2.one;
+
+    public LayerMask GroundMask;
+    public float GroundDistance = 1.1f;
+
+    public float IdleDrag = 1;
+    public float MovingDrag = 0;
+
+    public float AimOffsetScale = 5;
+
+    /** Jump effect. */
+    public GameObject JumpEffect;
+
+    /** Footstep effect. */
+    public GameObject FootstepEffect;
+
+    public Damageable Damageable
+    { get; private set; }
+
+    public float Health
+    { get { return Damageable.Health; } }
+
+    private Rigidbody _rigidbody;
+    private Animator _animator;
+
+    public bool Grounded { get; private set; }
+
+    private bool _jump;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+        Damageable = GetComponent<Damageable>();
+    }
+
+    void Update()
+    {
+        // Detect when player wishes to jump.
+        // Do this in Update, since it can be missed in FixedUpdate().
+        _jump = Input.GetButtonDown("Jump");
+        UpdateJump();
+    }
+
+    void FixedUpdate()
+    {
+        UpdateGrounded();
+        UpdateMovement();
+        UpdateJump();
+    }
+
+    void UpdateGrounded()
+    {
+        // Check if player is grounded.
+        Grounded = Physics.Raycast(transform.position, Vector3.down, GroundDistance, GroundMask);
+    }
+
+    void UpdateMovement()
+    {
+        // Check if player is dead.
+        if (Health <= 0)
+            return;
+
+        // Get input force scaling factor.
+        var scale = Grounded ? InputForceScale : AirborneForceScale;
+
+        // Get player's inputs.
+        var dx = Input.GetAxisRaw("Horizontal") * scale.x;
+        var dz = Input.GetAxisRaw("Vertical") * scale.y;
+        var idle = Mathf.Approximately(dx, 0) && Mathf.Approximately(dz, 0) && Grounded;
+
+        // Ajust player's drag depending on whether they wish to move or not.
+        _rigidbody.drag = idle ? IdleDrag : MovingDrag;
+
+        // Update player's movement force from inputs.
+        var velocity = _rigidbody.velocity;
+        var speed = velocity.magnitude;
+
+        var f = Vector3.zero;
+        if (Mathf.Sign(dx) != Mathf.Sign(velocity.x))
+            f.x += dx;
+        else
+            f.x += dx * (1 - Mathf.Clamp01(Mathf.Abs(velocity.x) / MaxSpeed));
+
+        if (Mathf.Sign(dz) != Mathf.Sign(velocity.z))
+            f.z += dz;
+        else
+            f.z += dz * (1 - Mathf.Clamp01(Mathf.Abs(velocity.z) / MaxSpeed));
+
+        f.x += -velocity.x * OpposingForceScale.x;
+        f.z += -velocity.z * OpposingForceScale.y;
+
+        _rigidbody.AddForce(f);
+        Debug.DrawRay(transform.position, f, Color.green);
+
+        // Update animator state.
+        // _animator.SetFloat("RunSpeed", Input.GetAxis("Horizontal"));
+    }
+
+    void UpdateJump()
+    {
+        // Check if player is dead.
+        if (Health <= 0)
+            return;
+
+        // Check if player wishes to jump and is grounded.
+        if (!_jump || !Grounded)
+            return;
+
+        // Apply jump force.
+        _rigidbody.AddForce(Vector2.up * JumpForce);
+
+        // Inform animator that jump has occurred.
+        if (_animator)
+            _animator.SetTrigger("Jump");
+
+        // Play jump effect.
+        if (JumpEffect)
+        {
+            var effect = Instantiate(JumpEffect, transform.position, transform.rotation) as GameObject;
+            effect.transform.parent = transform.transform;
+        }
+
+        // Don't jump until next frame.
+        _jump = false;
+    }
+
+    void Footstep()
+    {
+        if (!Grounded)
+            return;
+
+        if (FootstepEffect)
+        {
+            var effect = Instantiate(FootstepEffect, transform.position, transform.rotation) as GameObject;
+            effect.transform.parent = transform.transform;
+        }
+    }
 }
